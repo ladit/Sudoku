3ce2a76aeb3b1f266b90757f0af505dce80db443
Ladit <ladit7@gmail.com>
2017-09-10 18:56:02 +0800

    minor improvement.

--- src/sudokuMaker.h
+++ src/sudokuMaker.h
@@ -13,7 +13,7 @@ class sudokuMaker {
     int	blank;  // 数独棋盘待填格数，新的待填数独棋盘模板初始化后有 72 格待填
     int	hint;  // 数独棋盘提示格数
     int fullBoard[9][9];  // 完整数独棋盘
-	fileController fc;
+	  fileController fc;
     void newTemplate();
     bool dfs();
     int validNumbersQuantity(const int &row, const int &col, int (&mark)[10]);
